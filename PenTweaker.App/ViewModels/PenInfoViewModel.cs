514e8f5ddcfb84fe046b60d7b749d15ed5297a3f
Dag Ole Kristoffersen <thedole@gmail.com>
2017-12-17 01:04:00 +0100

    Added dubious viewmodel for the pendata

---
+++ PenTweaker.App/ViewModels/PenInfoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PenTweaker.App.ViewModels
+{
+    class PenInfoViewModel : INotifyPropertyChanged
+    {
+        private int _pressure = 0;
+
+        public int Pressure {
+            get { return _pressure;  }
+            set {
+                _pressure = value;
+                OnPropertyChanged();
+            }
+        }
+
+    #region plumbing
+    public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+    public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+        this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
+}
+}
